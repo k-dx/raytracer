6f25514fcd671bf9699d0b86614f9b9e74b7e19d
Christian Baldus <ChristianBaldus@web.de>
2024-10-29 06:03:15 +0100

    Fixed sphere

--- src/shapes/sphere.cpp
+++ src/shapes/sphere.cpp
@@ -6,22 +6,64 @@ public:
     Sphere(const Properties &properties) {
         
     }
-    bool intersect(const Ray &ray, Intersection &its,
-                   Sampler &rng) const override {
-        float dd = ray.direction.dot(ray.direction);
-        float od = Vector(ray.origin).dot(ray.direction);
-        float oo = Vector(ray.origin).dot(Vector(ray.origin));
-        return (od * od - 4 * dd * oo) > Epsilon;
+
+    inline void populate(SurfaceEvent &surf, const Point &position) const {
+        surf.position = position;
+
+        surf.uv.x() = (atan2f(position.z(), position.x()) + std::numbers::pi) / (2 * std::numbers::pi);
+        surf.uv.y() = (position.y() + 1) / 2;
+
+        // normal should already be normalized,, but do it again to get rid of potentially accumulated error
+        surf.shadingNormal  = Vector(position).normalized();
+        surf.geometryNormal = surf.shadingNormal;
+
+        //Three different cases to account for all edge cases where the normal in one direction is 0
+        //0.5 is approximately 1/sqrt(3). More accuracy is not needed here :)
+        if (position.x() >= 0.5)
+            surf.tangent = Vector((-position.y() - position.z()) / position.x(), 1.0f, 1.0f).normalized();
+        else if (position.y() >= 0.5)
+            surf.tangent = Vector(1.0f, (-position.x() - position.z()) / position.y(), 1.0f).normalized();
+        else
+            surf.tangent = Vector(1.0f, 1.0f, (-position.x() - position.y()) / position.z()).normalized();
+
+        surf.pdf = 0.0f;
     }
+
+    bool intersect(const Ray &ray, Intersection &its, Sampler &rng) const override {
+        const float od = Vector(ray.origin).dot(ray.direction);
+        const float oo = Vector(ray.origin).dot(Vector(ray.origin));
+        //since the ray direction is normalized, we have dd=1
+        const float radicant = od * od - oo + 1;
+        if (radicant <= 0.0f){
+            return false;
+        }
+        const float root = sqrt(radicant);
+    
+        const float t1 = -od + root; 
+        const float t2 = -od - root;
+        const float t = (t2 < t1 && t2 > Epsilon) || t1 <= Epsilon ? t2 : t1;
+        if (t < Epsilon || t > its.t){
+            return false;
+        }
+
+        its.t = t;
+        populate(its, ray(t));
+
+        return true;
+    }
+
     Bounds getBoundingBox() const override {
         return Bounds(Point(-1, -1, -1), Point(1, 1, 1));
     }
+
     Point getCentroid() const override {
         return Point(0);
     }
+
     AreaSample sampleArea(Sampler &rng) const override {
         NOT_IMPLEMENTED
     }
+
     std::string toString() const override { return "Sphere[]"; }
 };
 } // namespace lightwave
