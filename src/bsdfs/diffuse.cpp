f1949d5dce468e26fbf83cc9e38e30b3562b8b0d
Jakub Dubiel <jadu00002@stud.uni-saarland.de>
2024-12-04 12:01:29 +0100

    diffuse BSDF cleanup

--- src/bsdfs/diffuse.cpp
+++ src/bsdfs/diffuse.cpp
@@ -20,24 +20,15 @@ public:
 
     BsdfSample sample(const Point2 &uv, const Vector &wo,
                       Sampler &rng) const override {
-        // const Vector wi = squareToUniformHemisphere(rng.next2D());
-        const Vector wi = squareToCosineHemisphere(rng.next2D());
+        const Vector wi       = squareToCosineHemisphere(rng.next2D());
+        const float cos_theta = Frame::cosTheta(wi);
 
-        const float cos_theta = wi[2];
-        Color weight          = Color::white();
-        if (cosineHemispherePdf(wi) != 0.f) {
-            weight = evaluate(uv, wo, wi).value * cos_theta /
-                     cosineHemispherePdf(wi);
-            // uniformHemispherePdf();
+        if (cosineHemispherePdf(wi) == 0.f) {
+            return BsdfSample::invalid();
         }
 
-        assert_condition(wi.length() < 1.f + Epsilon &&
-                             wi.length() > 1.f - Epsilon, );
-        assert_condition(!std::isnan(wi[0]), );
-        assert_condition(!std::isnan(wi[1]), );
-        assert_condition(!std::isnan(wi[2]), );
-
-        assert_condition(!std::isnan(wi.length()), );
+        const Color weight =
+            evaluate(uv, wo, wi).value * cos_theta / cosineHemispherePdf(wi);
 
         return {
             .wi     = wi,
